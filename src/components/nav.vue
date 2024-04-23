47d6a36c0f6ec7b793d4f332e830230cd4642051
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-04-23 14:49:19 +0200

    ...

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -22,41 +22,6 @@
 export default {
   name: "NavBar",
   }
-  components:{
-    
-  },
-
-  methods: {
-    async redirectToStripe() {
-  console.log("HI");
-    fetch('http://localhost:8080/create-checkout-session', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        items: [
-          { id: 1, quantity: 1 }, 
-        ],
-      }),
-    })
-      .then(res => {
-     
-        if (res.ok) return res.json();
-        return res.json().then(json => Promise.reject(json));
-        
-      })
-      .then(({ url }) => {
-        console.log("Hi");
-      
-        window.location = url;
-      })
-      .catch(e => {
-        console.error(e.error);
-      });
-  }
-  }
-};
 </script>
 
 <style scoped>

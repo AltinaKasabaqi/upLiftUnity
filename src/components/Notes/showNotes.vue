b253d55283864982fcf867154d34ca7919fa378a
Leonita02 <leonitaa.selmani@gmail.com>
2024-05-10 23:29:46 +0200

    notes page for adding notes and showing them for the user

---
+++ src/components/Notes/showNotes.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+      <h1>Notes Chat</h1>
+      <NotesChat />
+    </div>
+  </template>
+  
+  <script>
+  import NotesChat from './addNotesForm.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      NotesChat
+    }
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -3,101 +3,106 @@ import authorizeMiddleware from '../authorization/auth';
 
 const routes = [
   {
-      path: "/",
-      name: "HomePage",
-      component: () => import("@/views/HomePage.vue"),
-       meta: { requiresAuth: false } 
-    },
-    {
+    path: "/",
+    name: "HomePage",
+    component: () => import("@/views/HomePage.vue"),
+    meta: { requiresAuth: false }
+  },
+  {
     path: '/myDashboard',
     name: 'AdminDashboard',
     component: () => import("@/dashboards/profilePage.vue"),
-    meta: { requiresAuth: true, requiredRoles: ['SuperAdmin','SuperVisor','Volunteer'] },
+    meta: { requiresAuth: true, requiredRoles: ['SuperAdmin', 'SuperVisor', 'Volunteer'] },
     children: [
       {
         path: '',
         name: 'Statistics',
         component: () => import("@/dashboards/statistics/statisticsPage.vue"),
       },
-    {
-      path: "/chat",
-      name: "LiveChat",
-      component: () => import("@/components/liveChat.vue"),
-      meta: { requiresAuth: true, requiredRoles: ['SuperAdmin','SuperVisor','Volunteer']}
-    },
-    {
-      path: "/register",
-      name: "SignUpForm",
-      component: () => import("@/views/SignUpForm.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    },
-    {
-      path: "/allUsersView",
-      name: "AllUsers",
-      component: () => import("@/dashboards/allUsersDash.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    
-    },  
-    {
-      path: "/UpdateUser:id?",
-      name: "UpdateUser",
-      component: () => import("@/dashboards/updateUser.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    },
-  
-    {
-      path: "/applications",
-      name: "ApplicationDash",
-      component: () => import("@/dashboards/applicationDash.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    },
-    {
-      path: "/donations",
-      name: "DonationsDash",
-      component: () => import("@/dashboards/donationDash.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    },
-    {
-      path: "/userActivities",
-      name: "UserActivities",
-      component: () => import("@/dashboards/userActivities.vue"),
-     
-    },{
-      path: "/callRate",
-      name: "CallRiskRate",
-      component: () => import("@/components/Calls/callRiskRating.vue"),
-      meta: { requiresAuth: true, requiredRoles: ['SuperAdmin','SuperVisor','Volunteer']}
-    },
-    {
-      path: "/calendar",
-      name: "CalendarV",
-      component: () => import("@/Calendar/calendar.vue"),
-      meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
-    
-    }
-  
-  ]
+      {
+        path: "/chat",
+        name: "LiveChat",
+        component: () => import("@/components/liveChat.vue"),
+        meta: { requiresAuth: true, requiredRoles: ['SuperAdmin', 'SuperVisor', 'Volunteer'] }
+      },
+      {
+        path: "/register",
+        name: "SignUpForm",
+        component: () => import("@/views/SignUpForm.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+      },
+      {
+        path: "/allUsersView",
+        name: "AllUsers",
+        component: () => import("@/dashboards/allUsersDash.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+
+      },
+      {
+        path: "/UpdateUser:id?",
+        name: "UpdateUser",
+        component: () => import("@/dashboards/updateUser.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+      },
+
+      {
+        path: "/applications",
+        name: "ApplicationDash",
+        component: () => import("@/dashboards/applicationDash.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+      },
+      {
+        path: "/donations",
+        name: "DonationsDash",
+        component: () => import("@/dashboards/donationDash.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+      },
+      {
+        path: "/userActivities",
+        name: "UserActivities",
+        component: () => import("@/dashboards/userActivities.vue"),
+
+      }, {
+        path: "/callRate",
+        name: "CallRiskRate",
+        component: () => import("@/components/Calls/callRiskRating.vue"),
+        meta: { requiresAuth: true, requiredRoles: ['SuperAdmin', 'SuperVisor', 'Volunteer'] }
+      },
+      {
+        path: "/calendar",
+        name: "CalendarV",
+        component: () => import("@/Calendar/calendar.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'SuperAdmin' }
+
+      },
+      {
+        path: "/addNotes",
+        name: "MyNotes",
+        component: () => import("@/components/Notes/addNotesForm.vue"),
+        meta: { requiresAuth: true, requiredRoles: 'Volunteer' }
+      },
+    ]
   },
 
 
-    
-  { 
-      path: "/Login",
-      name: "LoginForma",
-      component: () => import("@/views/LogIn.vue")  
+
+  {
+    path: "/Login",
+    name: "LoginForma",
+    component: () => import("@/views/LogIn.vue")
   },
   {
-    path: "/applicationForm", 
+    path: "/applicationForm",
     name: "ApplicationForm",
     component: () => import("@/views/VAplicationForm.vue"),
-    
+
   },
   {
     path: "/donationPackages",
     name: "DonationPackages",
     component: () => import("@/components/donationPackages.vue"),
   },
- 
+
 ];
 
 

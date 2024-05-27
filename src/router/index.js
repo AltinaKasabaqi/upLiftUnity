7f50cbf2665f6462e4b37c239a856d9e25069f14
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-05-27 18:24:22 +0200

    Changes in mySchedule dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const routes = [
         path: "/chat",
         name: "LiveChat",
         component: () => import("@/components/liveChat.vue"),
-        meta: { requiresAuth: true, requiredRoles: ['SuperAdmin', 'SuperVisor', 'Volunteer'] }
+        meta: { requiresAuth: true, requiredRoles: ['SuperVisor', 'Volunteer'] }
       },
       {
         path: "/register",

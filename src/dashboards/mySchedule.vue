aee7a647cb6e7ea79045edd2799900d010fdc7fd
diellzaaa1 <diellzasalihu10@gmail.com>
2024-05-27 19:17:37 +0200

    ...

--- src/dashboards/mySchedule.vue
+++ src/dashboards/mySchedule.vue
@@ -11,25 +11,27 @@
         <td><b>Termini 1</b></td>
         <td>{{ formatDate(schedule.firstDate) }}</td>
         <td>{{ getDayOfWeek(schedule.firstDate) }}</td>
-        <td>08:00 AM</td>
+        <td>{{ formatTime(schedule.firstDate) }}</td>
       </tr>
       <tr>
         <td><b>Termini 2</b></td>
         <td>{{ formatDate(schedule.secondDate) }}</td>
         <td>{{ getDayOfWeek(schedule.secondDate) }}</td>
-        <td>10:45 AM</td>
+        <td>{{ formatTime(schedule.secondDate) }}</td>
+
       </tr>
       <tr>
         <td><b>Termini 3</b></td>
         <td>{{ formatDate(schedule.thirdDate) }}</td>
         <td>{{ getDayOfWeek(schedule.thirdDate) }}</td>
-        <td>02:00 PM</td>
+        <td>{{ formatTime(schedule.thirdDate) }}</td>
       </tr>
       <tr>
         <td><b>Termini 4</b></td>
         <td>{{ formatDate(schedule.fourthDate) }}</td>
         <td>{{ getDayOfWeek(schedule.fourthDate) }}</td>
-        <td>04:45 PM</td>
+        <td>{{ formatTime(schedule.fourthDate) }}</td>
+
       </tr>
     </table>
   </div>
@@ -71,81 +73,15 @@ export default {
       console.error('Gabim gjatë marrjes së të dhënave:', error);
     });
 },
-
-    formatDate(dateString) {
-        <th>Termini</th>
-        <th>Data</th>
-        <th>Dita</th>
-        <th>Koha</th>
-      </tr>
-      
-      <tr>
-        <td><b>Termini 1</b></td>
-        <td>{{ formatDate(schedule.firstDate) }}</td>
-        <td>{{ getDayOfWeek(schedule.firstDate) }}</td>
-        <td>{{ formatTime(schedule.firstDate) }}</td>
-      </tr>
-      <tr>
-        <td><b>Termini 2</b></td>
-        <td>{{ formatDate(schedule.secondDate) }}</td>
-        <td>{{ getDayOfWeek(schedule.secondDate) }}</td>
-        <td>{{ formatTime(schedule.secondDate) }}</td>
-      </tr>
-      <tr>
-        <td><b>Termini 3</b></td>
-        <td>{{ formatDate(schedule.thirdDate) }}</td>
-        <td>{{ getDayOfWeek(schedule.thirdDate) }}</td>
-        <td>{{ formatTime(schedule.thirdDate) }}</td>
-      </tr>
-      <tr>
-        <td><b>Termini 4</b></td>
-        <td>{{ formatDate(schedule.fourthDate) }}</td>
-        <td>{{ getDayOfWeek(schedule.fourthDate) }}</td>
-        <td>{{ formatTime(schedule.fourthDate) }}</td>
-      </tr>
-    </table>
-  </div>
-</template>
-
-<script>
-import axios from 'axios';
-import { getUserIdFromToken } from '../authorization/authUserId';
-
-export default {
-  data() {
-    return {
-      userId: '',
-      schedule: {}
-    };
-  },
-  mounted() {
-    this.userId = getUserIdFromToken();
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      axios.get('http://localhost:5051/api/Schedule/GetScheduleByUserId', {
-          params: {
-            userId: this.userId
-          }
-        })
-        .then(response => {
-          this.schedule = response.data;
-          console.log(this.schedule)
-        })
-        .catch(error => {
-          console.error('Gabim gjatë marrjes së të dhënave:', error);
-        });
-    },
-    formatDate(dateString) {
+  formatDate(dateString) {
       const date = new Date(dateString);
       return date.toLocaleDateString('sq-AL', { day: 'numeric', month: 'long', year: 'numeric' });
     },
-    getDayOfWeek(dateString) {
-      const daysOfWeek = ['E Diel', 'E Hënë', 'E Martë', 'E Mërkurë', 'E Enjte', 'E Premte', 'E Shtunë'];
-      const date = new Date(dateString);
-      return daysOfWeek[date.getDay()];
-    },
+      getDayOfWeek(dateString) {
+        const daysOfWeek = ['E Diel', 'E Hënë', 'E Martë', 'E Mërkurë', 'E Enjte', 'E Premte', 'E Shtunë'];
+        const date = new Date(dateString);
+        return daysOfWeek[date.getDay()];
+      },
     formatTime(dateString) {
       const date = new Date(dateString);
       return date.toLocaleTimeString('sq-AL', { hour: '2-digit', minute: '2-digit' });

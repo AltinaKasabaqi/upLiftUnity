524b49c8ba3b2b1ac08db706e8244602d90cc75f
diellzaaa1 <diellzasalihu10@gmail.com>
2024-05-28 15:40:55 +0200

    ..

--- src/tests/testRegisterStaff.js
+++ src/tests/testRegisterStaff.js
@@ -3,24 +3,39 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 async function testRegisterStaff() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-        await driver.get('http://localhost:8080/myDashboard/register'); // Zëvendësoni me URL-në specifike të regjistrimit të stafit
+        await driver.get('http://localhost:8080/#/login');
 
-        // Prisni që elementi me ID 'emri' të jetë i pranishëm dhe i dukshëm
-        await driver.wait(until.elementLocated(By.id('emri')), 10000);
-        await driver.wait(until.elementIsVisible(driver.findElement(By.id('emri'))), 10000);
+        await driver.wait(until.elementLocated(By.id('email')), 20000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.id('email'))), 20000);
 
-        // Plotësoni formularin e regjistrimit
+        await driver.findElement(By.id('email')).sendKeys('admin2@gmail.com'); 
+        await driver.findElement(By.id('password')).sendKeys('admini'); 
+        await driver.findElement(By.xpath("//button[text()='Kyçu']")).click();
+
+        await driver.wait(until.urlIs('http://localhost:8080/#/statistics'), 10000);
+
+        await driver.get('http://localhost:8080/#/register'); 
+
+        await driver.wait(until.elementLocated(By.id('emri')), 20000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.id('emri'))), 20000);
+
         await driver.findElement(By.id('emri')).sendKeys('TestEmri');
         await driver.findElement(By.id('mbiemri')).sendKeys('TestMbiemri');
         await driver.findElement(By.id('email')).sendKeys('testemail@example.com');
         await driver.findElement(By.id('fjalekalimi')).sendKeys('TestFjalekalimi123');
         await driver.findElement(By.id('phoneNumber')).sendKeys('123456789');
         await driver.findElement(By.id('address')).sendKeys('Test Adresa');
-        await driver.findElement(By.id('roli')).sendKeys('2'); // Mbikqyrës
 
-        await driver.findElement(By.xpath("//button[text()='Regjistrohu']")).click();
+        let roleSelect = await driver.findElement(By.id('roli'));
+        await roleSelect.click();
+        await roleSelect.sendKeys(Key.ARROW_DOWN); 
+        await roleSelect.sendKeys(Key.ENTER); 
 
-        await driver.sleep(2000);
+        let registerButton = await driver.findElement(By.xpath("//button[text()='Regjistrohu']"));
+        await driver.wait(until.elementIsVisible(registerButton), 20000);
+        await registerButton.click();
+
+        await driver.sleep(3000);
 
         let bodyText = await driver.findElement(By.tagName('body')).getText();
         if (bodyText.includes('Përdoruesi u regjistrua me sukses!')) {
@@ -34,6 +49,5 @@ async function testRegisterStaff() {
         await driver.quit();
     }
 }
-
 
 testRegisterStaff();

f23ff16336e99295324009bfd26554744b875a0e
Leonita02 <leonitaa.selmani@gmail.com>
2024-05-28 17:52:46 +0200

    testing rule form with selenium

---
+++ src/tests/testRuleForm.js
@@ -0,0 +1,46 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+async function testRuleForm() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('http://localhost:8080/#/login');
+
+        await driver.wait(until.elementLocated(By.id('email')), 20000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.id('email'))), 20000);
+
+        await driver.findElement(By.id('email')).sendKeys('admin@gmail.com'); 
+        await driver.findElement(By.id('password')).sendKeys('admin'); 
+        await driver.findElement(By.xpath("//button[text()='Kyçu']")).click();
+
+        await driver.wait(until.urlIs('http://localhost:8080/#/statistics'), 20000);
+
+        await driver.get('http://localhost:8080/#/RulesForma');
+
+        await driver.wait(until.elementLocated(By.id('name')), 20000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.id('name'))), 20000);
+
+        // Vendosni vlerat e fushave
+        await driver.findElement(By.id('name')).sendKeys('TestRuleName');
+        await driver.findElement(By.id('description')).sendKeys('Test Rule Description');
+
+        // Dorëzo formën duke klikuar butonin "Shto"
+        await driver.findElement(By.css('form button[type="submit"]')).click();
+
+        // Prit për një mesazh të suksesit ose mesazhin e gabimit
+        await driver.wait(until.alertIsPresent(), 5000);
+        let successAlert = await driver.switchTo().alert();
+        assert.strictEqual(await successAlert.getText(), 'Rregullat u shtuan me sukses!');
+
+        // Prit për shkarkimin e faqes së AdminDashboard pasi të kthehet nga formulari
+        await driver.wait(until.urlIs('http://localhost:8080/#/AdminDashboard'), 5000);
+
+        console.log('Formulari u testua me sukses!');
+    } catch (error) {
+        console.error('Gabim gjatë testimin e formës së regjistrimit të rregullave:', error);
+    } finally {
+        await driver.quit();
+    }
+}
+
+testRuleForm();
